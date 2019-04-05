6d22ee9905e9ab18ac2c0d71d902190b85ab18c6
Mark <caroldonahue@Carols-MBP-2.hsd1.wa.comcast.net>
2019-04-04 20:59:28 -0700

    adds documentation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   impressionist
   config.cache_store = :null_store
+  before_action :set_cache_buster
   
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
+
   def home
     UpdateCounterJob.perform_later
   end
